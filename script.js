b7b508bec297005cc7d9729db94e4dd9731fb67f
mdrifat59 <mdrifatulislam59@gmail.com>
2025-05-15 00:51:08 +0600

    quntity and price functionalty done

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 let mainus = document.querySelector(".mainus")
 let count = document.querySelector(".count")
-let plus = document.querySelector(".plus") 
+let plus = document.querySelector(".plus")
+let reguler = document.querySelector(".reguler")
+let discount = document.querySelector(".discount")
 
+let regprice = 396
+let disprice = 249
 let number = 1;
 
-mainus.addEventListener("click", function(){
-      if(number > 1){
-        number--;
-         count.innerHTML = number
-      }
-    
-})
-plus.addEventListener("click", function(){
-     if(number < 10){
-        number ++;
+reguler.innerHTML = regprice.toFixed(2)
+discount.innerHTML = disprice.toFixed(2)
+count.innerHTML = number
+
+plus.addEventListener("click", function () {
+    if (number < 10) {
+        number++;
+        reguler.innerHTML = (regprice * number).toFixed(2)
+        discount.innerHTML = (disprice * number).toFixed(2)
         count.innerHTML = number
-     }
-    
+    } 
+
+})
+mainus.addEventListener("click", function () {
+    if (number > 1) {
+        number--;
+        reguler.innerHTML = (reguler.innerHTML - regprice).toFixed(2)
+        discount.innerHTML = (discount.innerHTML - disprice).toFixed(2)
+        count.innerHTML = number
+    }
+
 })
